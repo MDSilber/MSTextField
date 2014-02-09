0be1f970128082527abac2812db52591fd2edfc5
Mason Silber <mds2161@columbia.edu>
2014-02-08 19:21:51 -0500

    Added view controller and checkmark asset

---
+++ MSTextField/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MSTextField
+//
+//  Created by Mason Silber on 2/8/14.
+//  Copyright (c) 2014 MasonSilber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MSTextField/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  MSTextField
+//
+//  Created by Mason Silber on 2/8/14.
+//  Copyright (c) 2014 MasonSilber. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "MSTextField.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor darkGrayColor];
+    
+    MSTextField *textField = [[MSTextField alloc] initWithFrame:CGRectMake(0, 50, 0, 0)];
+    [self.view addSubview:textField];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
