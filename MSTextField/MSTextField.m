a179cd1cac5fa78d0afa3d4d2ef6052dc00912d9
Mason Silber <mds2161@columbia.edu>
2014-02-09 22:49:20 -0500

    Added beginning of credit card field and automated length checking

--- MSTextField/MSTextField.m
+++ MSTextField/MSTextField.m
@@ -29,136 +29,6 @@
 
 @implementation MSTextField
 
-+(UIView *)paddingView
-{
-    UIView *paddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 20)];
-    return paddingView;
-}
-
-- (void)dealloc
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-- (id)initWithFrame:(CGRect)frame
-{
-    frame = CGRectMake(10, frame.origin.y, 300, 50);
-    self = [super initWithFrame:frame];
-    if (self)
-    {
-        [self setBackgroundColor:UIColorFromRGB(0xdedede)];
-        [self setTextColor:[UIColor blackColor]];
-        [self setFont:[UIFont fontWithName:@"HelveticaNeue-Bold" size:12.0f]];
-        [self setClearButtonMode:UITextFieldViewModeWhileEditing];
-        [[self layer] setCornerRadius:3.5f];
-        [self setLeftView:[MSTextField paddingView]];
-        [self setLeftViewMode:UITextFieldViewModeAlways];
-        [self setTextAlignment:NSTextAlignmentLeft];
-        [self setContentVerticalAlignment:UIControlContentVerticalAlignmentCenter];
-        
-        self.invalidInputBorderColor = UIColorFromRGB(0xB50000);
-        self.inputState = MSTextFieldUnknownInput;
-        self.minimumLengthToVerify = 1;
-        self.maxLengthOfInput = MAX_INPUT;
-
-        self.validInputImage = [UIImage imageNamed:@"green-checkmark"];
-        self.validInputImageView = [[UIImageView alloc] initWithImage:_validInputImage];
-        self.validInputImageView.frame = CGRectMake(self.frame.size.width - _validInputImage.size.width - 10, floorf((self.frame.size.height - _validInputImage.size.height)/2.0f), _validInputImage.size.width, _validInputImage.size.height);
-        self.validInputImageView.hidden = YES;
-        [self addSubview:self.validInputImageView];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textDidChange:) name:UITextFieldTextDidChangeNotification object:nil];
-    }
-    return self;
-}
-
--(void)drawPlaceholderInRect:(CGRect)rect
-{
-    if (IS_IOS7) {
-        CGSize placeHolderSize = [self.placeholder boundingRectWithSize:self.frame.size options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12.0f]} context:nil].size;
-        rect.origin.y = (self.frame.size.height - placeHolderSize.height)/2;
-        rect.size.height = placeHolderSize.height;
-    }
-    
-    NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
-    paragraphStyle.alignment = self.textAlignment;
-    paragraphStyle.lineBreakMode = NSLineBreakByClipping;
-    [self.placeholder drawInRect:rect withAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12.0f], NSParagraphStyleAttributeName: paragraphStyle, NSForegroundColorAttributeName: UIColorFromRGB(0x787878)}];
-}
-
-- (void)setInputState:(InputState)inputState
-{
-    if (self.inputState == inputState) {
-        return;
-    }
-    _inputState = inputState;
-    if (_inputState == MSTextFieldInvalidInput) {
-        self.layer.borderWidth = 2.0f;
-        self.validInputImageView.hidden = YES;
-        if ([self.delegate respondsToSelector:@selector(textFieldReceivedInvalidInput:)]) {
-            [self.delegate textFieldReceivedInvalidInput:self];
-        }
-    } else {
-        self.layer.borderWidth = 0.0f;
-        if (_inputState == MSTextFieldValidInput) {
-            self.validInputImageView.hidden = NO;
-            if ([self.delegate respondsToSelector:@selector(textFieldReceivedValidInput:)]) {
-                [self.delegate textFieldReceivedValidInput:self];
-            }
-        } else {
-            self.validInputImageView.hidden = YES;
-        }
-    }
-}
-
-- (void)setValidInputImage:(UIImage *)validInputImage
-{
-    if (_validInputImage != validInputImage) {
-        _validInputImage = validInputImage;
-        _validInputImageView.image = validInputImage;
-        _validInputImageView.frame = CGRectMake(self.frame.size.width - _validInputImage.size.width - 10, floorf((self.frame.size.height - _validInputImage.size.height)/2.0f), _validInputImage.size.width, _validInputImage.size.height);
-    }
-}
-
-- (void)setInvalidInputBorderColor:(UIColor *)invalidInputBorderColor
-{
-    if (_invalidInputBorderColor != invalidInputBorderColor) {
-        _invalidInputBorderColor = invalidInputBorderColor;
-        self.layer.borderColor = invalidInputBorderColor.CGColor;
-    }
-}
-
-#pragma mark - Text field methods
-
-- (void)textDidChange:(NSNotification *)notification
-{
-    if ([((UITextField *)(notification.object)).text length] > self.maxLengthOfInput) {
-        ((UITextField *)(notification.object)).text = self.textFieldString;
-    } else {
-        char newCharacter = ([self.text length] > [self.textFieldString length]) ? [self.text characterAtIndex:([self.text length] -1)] : '\b';
-        if (_formattingBlock) {
-            _formattingBlock(self, newCharacter);
-        }
-        self.textFieldString = ((UITextField *)(notification.object)).text;
-    }
-}
-
-- (BOOL)becomeFirstResponder
-{
-    self.inputState = MSTextFieldUnknownInput;
-    return [super becomeFirstResponder];
-}
-
-- (BOOL)resignFirstResponder
-{
-    if (_verificationBlock && [self.text length] >= self.minimumLengthToVerify) {
-        if (_verificationBlock(self.text)) {
-            self.inputState = MSTextFieldValidInput;
-        } else {
-            self.inputState = MSTextFieldInvalidInput;
-        }
-    }
-    return [super resignFirstResponder];
-}
-
 #pragma mark - Factory methods
 
 + (MSTextField *)phoneNumberFieldWithFrame:(CGRect)frame
@@ -251,6 +121,139 @@
         return NSLocationInRange([[text substringToIndex:2] intValue], NSMakeRange(0, 12));
     };
     return dateField;
+}
+
+#pragma mark - Class and instance methods
+
++(UIView *)paddingView
+{
+    UIView *paddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 20)];
+    return paddingView;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    frame = CGRectMake(10, frame.origin.y, 300, 50);
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        [self setBackgroundColor:UIColorFromRGB(0xdedede)];
+        [self setTextColor:[UIColor blackColor]];
+        [self setFont:[UIFont fontWithName:@"HelveticaNeue-Bold" size:12.0f]];
+        [self setClearButtonMode:UITextFieldViewModeWhileEditing];
+        [[self layer] setCornerRadius:3.5f];
+        [self setLeftView:[MSTextField paddingView]];
+        [self setLeftViewMode:UITextFieldViewModeAlways];
+        [self setTextAlignment:NSTextAlignmentLeft];
+        [self setContentVerticalAlignment:UIControlContentVerticalAlignmentCenter];
+        
+        self.invalidInputBorderColor = UIColorFromRGB(0xB50000);
+        self.inputState = MSTextFieldUnknownInput;
+        self.minimumLengthToVerify = 1;
+        self.maxLengthOfInput = MAX_INPUT;
+
+        self.validInputImage = [UIImage imageNamed:@"green-checkmark"];
+        self.validInputImageView = [[UIImageView alloc] initWithImage:_validInputImage];
+        self.validInputImageView.frame = CGRectMake(self.frame.size.width - _validInputImage.size.width - 10, floorf((self.frame.size.height - _validInputImage.size.height)/2.0f), _validInputImage.size.width, _validInputImage.size.height);
+        self.validInputImageView.hidden = YES;
+        [self addSubview:self.validInputImageView];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textDidChange:) name:UITextFieldTextDidChangeNotification object:nil];
+    }
+    return self;
+}
+
+-(void)drawPlaceholderInRect:(CGRect)rect
+{
+    if (IS_IOS7) {
+        CGSize placeHolderSize = [self.placeholder boundingRectWithSize:self.frame.size options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12.0f]} context:nil].size;
+        rect.origin.y = (self.frame.size.height - placeHolderSize.height)/2;
+        rect.size.height = placeHolderSize.height;
+    }
+    
+    NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+    paragraphStyle.alignment = self.textAlignment;
+    paragraphStyle.lineBreakMode = NSLineBreakByClipping;
+    [self.placeholder drawInRect:rect withAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12.0f], NSParagraphStyleAttributeName: paragraphStyle, NSForegroundColorAttributeName: UIColorFromRGB(0x787878)}];
+}
+
+- (void)setInputState:(InputState)inputState
+{
+    if (self.inputState == inputState) {
+        return;
+    }
+    _inputState = inputState;
+    if (_inputState == MSTextFieldInvalidInput) {
+        self.layer.borderWidth = 2.0f;
+        self.validInputImageView.hidden = YES;
+        if ([self.delegate respondsToSelector:@selector(textFieldReceivedInvalidInput:)]) {
+            [self.delegate textFieldReceivedInvalidInput:self];
+        }
+    } else {
+        self.layer.borderWidth = 0.0f;
+        if (_inputState == MSTextFieldValidInput) {
+            self.validInputImageView.hidden = NO;
+            if ([self.delegate respondsToSelector:@selector(textFieldReceivedValidInput:)]) {
+                [self.delegate textFieldReceivedValidInput:self];
+            }
+        } else {
+            self.validInputImageView.hidden = YES;
+        }
+    }
+}
+
+- (void)setValidInputImage:(UIImage *)validInputImage
+{
+    if (_validInputImage != validInputImage) {
+        _validInputImage = validInputImage;
+        _validInputImageView.image = validInputImage;
+        _validInputImageView.frame = CGRectMake(self.frame.size.width - _validInputImage.size.width - 10, floorf((self.frame.size.height - _validInputImage.size.height)/2.0f), _validInputImage.size.width, _validInputImage.size.height);
+    }
+}
+
+- (void)setInvalidInputBorderColor:(UIColor *)invalidInputBorderColor
+{
+    if (_invalidInputBorderColor != invalidInputBorderColor) {
+        _invalidInputBorderColor = invalidInputBorderColor;
+        self.layer.borderColor = invalidInputBorderColor.CGColor;
+    }
+}
+
+#pragma mark - Text field handling methods
+
+- (void)textDidChange:(NSNotification *)notification
+{
+    if ([((UITextField *)(notification.object)).text length] > self.maxLengthOfInput) {
+        ((UITextField *)(notification.object)).text = self.textFieldString;
+    } else {
+        char newCharacter = ([self.text length] > [self.textFieldString length]) ? [self.text characterAtIndex:([self.text length] -1)] : '\b';
+        if (_formattingBlock) {
+            _formattingBlock(self, newCharacter);
+        }
+        self.textFieldString = ((UITextField *)(notification.object)).text;
+    }
+}
+
+- (BOOL)becomeFirstResponder
+{
+    self.inputState = MSTextFieldUnknownInput;
+    return [super becomeFirstResponder];
+}
+
+- (BOOL)resignFirstResponder
+{
+    if (_verificationBlock && [self.text length] >= self.minimumLengthToVerify) {
+        if (_verificationBlock(self.text)) {
+            self.inputState = MSTextFieldValidInput;
+        } else {
+            self.inputState = MSTextFieldInvalidInput;
+        }
+    }
+    return [super resignFirstResponder];
 }
 
 @end
